e4138b28f170d5d30248513e826bc16fff6681d0
Allyssa Deanna <dee@halalfoodhunt.com>
2017-05-29 09:51:45 +0000

    css

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 def sign_up_params
-		params.require(:merchant).permit(:name, :email, :password)
+		params.require(:merchant).permit(:name, :email, :password, :author_profile)
 	end
 	
 	def account_update_params
-		params.require(:merchant).permit(:name, :email, :password)
+		params.require(:merchant).permit(:name, :email, :password, :author_profile)
 	end
 end
