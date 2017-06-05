1ee04fe635dce0183ef25600c3b29b54d9871d37
Allyssa Deanna <dee@halalfoodhunt.com>
2017-06-05 15:18:10 +0000

    css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,6 @@
 module ApplicationHelper
-def avatar_url(user)
-  if user.avatar_url.present?
-    user.avatar_url
-  else
-    default_url = "#{root_url}images/guest.png"
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+  +def gravatar_url(email, size)
+    gravatar = Digest::MD5::hexdigest(email).downcase
+    url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}"
   end
-end
 end
