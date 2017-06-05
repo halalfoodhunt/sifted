a7a1613d4d717417e587a794df766d413b112acb
Allyssa Deanna <dee@halalfoodhunt.com>
2017-06-05 15:21:04 +0000

    css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  +def gravatar_url(email, size)
+  def gravatar_url(email, size)
     gravatar = Digest::MD5::hexdigest(email).downcase
     url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}"
   end
