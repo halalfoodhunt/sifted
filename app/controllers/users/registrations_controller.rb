dd1f402467e32f0e0710837a22f5ed4d81fd6711
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-06 09:45:59 +0000

    edited

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,4 +6,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	def account_update_params
 		params.require(:user).permit(:name, :email, :password, :author_profile, :avatar)
 	end
+	
+	def update_resource(resource, params)
+      if params[:password].blank? && params[:password_confirmation].blank? && params[:current_password].blank?
+      resource.update_without_password(params)
+      else
+        super
+      end
+    end
 end
