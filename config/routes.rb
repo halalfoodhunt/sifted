944ee0d3fdbfdcbd22e7613fde2473d44c857880
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-13 09:07:41 +0000

    edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :categories
   mount Ckeditor::Engine => '/ckeditor'
   
   root to: "posts#index"
   resources :posts
+  resources :categories
   devise_for :users, controllers: { registrations: "users/registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
