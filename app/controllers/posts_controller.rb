07e520789ae718fe05bfab5faab220514e7051f9
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-13 08:23:01 +0000

    edited

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -90,6 +90,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :featured_image, :article_url)
+      params.require(:post).permit(:title, :content, :featured_image, :article_url, :draft, :date_published)
     end
 end
---
+++ db/migrate/20170713080617_add_date_published_to_posts.rb
@@ -0,0 +1,5 @@
+class AddDatePublishedToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :date_published, :datetime
+  end
+end
