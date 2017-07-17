88fbc6f2c078254d964d0ed98d5c64d770b0317f
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-17 03:12:22 +0000

    edited

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,4 +1,4 @@
 class Medium < ActiveRecord::Base
     has_attached_file :media_upload, styles: { large: "1920x500#", medium: "800x500#", square: "500x500#", thumb: "100x67>" }, default_url: "/images/:style/image-placeholder.jpg"
-    validates_attachment_content_type :featured_image, content_type: /\Aimage\/.*\Z/
+    validates_attachment_content_type :media_upload, content_type: /\Aimage\/.*\Z/
 end
