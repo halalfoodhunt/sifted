f324ad8b368e8e65413be0daf6c079c37ed2b9fd
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-20 03:07:03 +0000

    edited

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
 
   root to: "posts#index"
-  get ':title', to: 'posts#show'
+  get 'posts(/:id)', to: :display
   resources :posts
   resources :categories
   devise_for :users, controllers: { registrations: "users/registrations" }
