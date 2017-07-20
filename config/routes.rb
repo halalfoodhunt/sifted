09973cf8fea521ce5baf5e54bae5320ac42b305a
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-20 03:34:10 +0000

    edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   root to: "posts#index"
-  get ':title', to: 'posts#show', as: :post
+  get '/:id', to: 'posts#show'
   resources :posts
   resources :categories
   resources :media
