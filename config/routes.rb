593ed51b3ebe30eb9f519555739958cb3ed63341
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-20 23:31:31 +0000

    Added slugs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
   root to: "pages#index"
-  get 'pages/feedback_form', as: 'feedback_form'
+  get 'pages/feedback_form'
   get '/:id', to: 'posts#show'
   resources :categories
   resources :media
