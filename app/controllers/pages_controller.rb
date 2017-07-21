eb5eacc3f55ee3a8d324361b3754d1115ccb4eac
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-21 15:46:17 +0000

    Added slugs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
 			@posts = Post.all.order("date_published DESC")
 		else
 			@category_id = SubCategory.find_by(name: params[:sub_category]).id
-			@posts = Post.where(sub_category_id: @sub_category_id).order("created_at DESC")
+			@posts = Post.where(sub_category_id: @sub_category_id).order("date_published DESC")
 		end
     @meta_title = meta_title 'SIFTED - Behind the Halal Food Scene'
     @meta_description = 'Sifted is an online magazine to help halal foodies sift through the multiple food choice available out there, and to bring them more information behind the scenes of the halal food scene.'
