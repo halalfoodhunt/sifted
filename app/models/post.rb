6276af6ad3153cc2c8b093aa4ead4ad449874f06
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-24 14:57:45 +0000

    edited

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,6 @@ class Post < ActiveRecord::Base
    title_changed?
   end
   
-    has_attached_file :featured_image, styles: { portrait: "200x420#", large: "1920x500#", medium: "800x500#", square: "500x500#", thumb: "100x67>" }, default_url: "/images/:style/image-placeholder.jpg"
+    has_attached_file :featured_image, styles: { portrait: "120x420#", large: "1920x500#", medium: "800x500#", square: "500x500#", thumb: "100x67>" }, default_url: "/images/:style/image-placeholder.jpg"
     validates_attachment_content_type :featured_image, content_type: /\Aimage\/.*\Z/
 end
