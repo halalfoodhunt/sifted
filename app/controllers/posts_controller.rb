32caf08d7f17ada5d5eecbc0e2619a019c4625ff
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-24 12:54:39 +0000

    edited

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -90,6 +90,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :featured_image, :article_url, :draft, :date_published, :category_id, :sub_category_id, :post_snippet, :url_slug)
+      params.require(:post).permit(:title, :content, :featured_image, :article_url, :draft, :date_published, :category_id, :sub_category_id, :post_snippet, :url_slug, :featured_1, :featured_2, :featured_3)
     end
 end
