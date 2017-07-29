fd565aee301d5ea83d1bcf0c2b9eb2c3eda784fc
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-29 15:33:44 +0000

    edited

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -69,6 +69,6 @@ class AdsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def ad_params
-      params.require(:ad).permit(:name, :commencement_date, :expiry_date, :publish)
+      params.require(:ad).permit(:name, :commencement_date, :expiry_date, :publish, :image)
     end
 end
--- app/models/advertisement.rb
+++
@@ -1,4 +0,0 @@
-class Advertisement < ActiveRecord::Base
-    has_attached_file :image, styles: { portrait: "350x420#", large: "1920x500#", medium: "800x500#", square: "500x500#", thumb: "100x67>" }, default_url: "/images/:style/image-placeholder.jpg"
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-end
