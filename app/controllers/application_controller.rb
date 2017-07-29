6758bc351863e1dbed7b56eef55493612113093c
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-29 17:56:34 +0000

    edited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
-  flash[:error] = "Access denied."
-  redirect_to root_url
+    redirect_to main_app.root_path, :alert => exception.message
   end
+  
   BRAND_NAME = 'SIFTED - Behind the Halal Food Scene'.freeze
 
   def meta_title(title)
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,6 +1,13 @@
 RailsAdmin.config do |config|
   
-  config.authorize_with :cancan #TODO add cancancan to rails_admin config
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
+    
+  config.authorize_with do
+    redirect_to main_app.root_path unless current_user.admin?
+  end
 
   ### Popular gems integration
 
