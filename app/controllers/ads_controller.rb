1cc5d4ef54eef79e8a1c5f4f2017b2738abc0f44
Allyssa Deanna <dee@halalfoodhunt.com>
2017-07-30 14:47:36 +0000

    EDITED

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -77,6 +77,6 @@ class AdsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def ad_params
-      params.require(:ad).permit(:name, :commencement_date, :expiry_date, :publish, :image)
+      params.require(:ad).permit(:name, :commencement_date, :expiry_date, :publish, :image, :link)
     end
 end
