8f3506da4817642dd0e8859a9cbc7af58e970038
Allyssa Deanna <dee@halalfoodhunt.com>
2017-08-18 08:38:47 +0000

    edited

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,10 @@ class PostsController < ApplicationController
   def index
     @sub_category = SubCategory.all
     if params[:sub_category].blank?
-			@posts = Post.where(draft: false).order("date_published DESC")
+			@posts = Post.where(draft: false).order("date_published DESC").limit(8)
 		else
 			@sub_category_id = SubCategory.find_by(name: params[:sub_category]).id
-			@posts = Post.where(sub_category_id: @sub_category_id).order("created_at DESC")
+			@posts = Post.where(sub_category_id: @sub_category_id).order("created_at DESC").where(draft: false)
 		end
     @meta_title = meta_title 'Sifted by HALALFOODHUNT'
     @meta_description = 'Sifted is an online magazine to help halal foodies sift through the multiple food choice available out there, and to bring them more information behind the scenes of the halal food scene.'
